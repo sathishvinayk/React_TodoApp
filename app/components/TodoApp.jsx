0dea300246944ed20d233133d65aaa565119cf43
sathishvinayk <sathishvinayk@gmail.com>
2017-03-22 21:15:34 +0530

    id updated

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -12,17 +12,21 @@ var TodoApp=React.createClass({
       todos:[
         {
           id:uuid(),
-          text:'Walk the dog'
+          text:'Walk the dog',
+          completed: false
         },{
           id:uuid(),
-          text: 'Clean the yard'
+          text: 'Clean the yard',
+          completed:true
         },
         {
           id:uuid(),
-          text:'lean on cat'
+          text:'lean on cat',
+          completed:true
         },{
           id:uuid(),
-          text:'Run aroung'
+          text:'Run around',
+          completed: false
         }
       ]
     };
@@ -33,7 +37,8 @@ var TodoApp=React.createClass({
         ...this.state.todos,
         {
           id:uuid(),
-          text: text
+          text: text,
+          completed:false
         }
       ]
     })
@@ -44,12 +49,20 @@ var TodoApp=React.createClass({
       searchText:searchText.toLowerCase()
     })
   },
+  handleToggle:function(id){
+    var updatedTodos=this.state.todos.map((todo)=>{
+      if(todo.id===id){
+         todo.completed=!todo.completed;
+      }
+    });
+    this.setState({todos:updatedTodos});
+  },
   render: function(){
     var {todos}=this.state;
     return (
       <div>
         <TodoSearch onSearch={this.handleSearch} />
-        <TodoList todos={todos} />
+        <TodoList todos={todos} onToggle={this.handleToggle} />
         <AddTodo onAddTodo={this.handleAddTodo} />
       </div>
     )
