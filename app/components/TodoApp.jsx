e707f1481a7b5d06e9bfc787c0fa07f05f9da34d
sathishvinayk <sathishvinayk@gmail.com>
2017-03-22 20:42:46 +0530

    Made uuid

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,4 +1,5 @@
 var React=require('react');
+var uuid=require('node-uuid');
 var TodoList=require('TodoList');
 var AddTodo=require('AddTodo');
 var TodoSearch=require('TodoSearch');
@@ -10,24 +11,32 @@ var TodoApp=React.createClass({
       searchText: '',
       todos:[
         {
-          id:1,
+          id:uuid(),
           text:'Walk the dog'
         },{
-          id:2,
+          id:uuid(),
           text: 'Clean the yard'
         },
         {
-          id:3,
+          id:uuid(),
           text:'lean on cat'
         },{
-          id:4,
+          id:uuid(),
           text:'Run aroung'
         }
       ]
     };
   },
   handleAddTodo:function(text){
-    alert('New todo: '+text)
+    this.setState({
+      todos:[
+        ...this.state.todos,
+        {
+          id:uuid(),
+          text: text
+        }
+      ]
+    })
   },
   handleSearch:function(showCompleted, searchText){
     this.setState({
